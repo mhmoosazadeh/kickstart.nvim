47f116ddbdec54360892828987cf5f4b8728fe8b
Mohammad Moosazadeh <mohammad@jewlr.com>
2026-01-07 13:38:20 -0500

    Set shortcut to copy current file path

--- init.lua
+++ init.lua
@@ -229,6 +229,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.keymap.set('n', '<leader>cp', ':let @+=expand("%")<CR>', { desc = 'Copy relative path to clipboard' })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
